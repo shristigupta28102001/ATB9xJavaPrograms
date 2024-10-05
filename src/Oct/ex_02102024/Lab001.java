cf47eef0ebe5dd6d3a26d4cb879614ae3657721b
Shristi gupta <shristig776@gmail.com>
2024-10-05 23:26:05 +0530

    committing the changes

--- src/Oct/ex_02102024/Lab001.java
+++ src/Oct/ex_02102024/Lab001.java
@@ -4,5 +4,6 @@ public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("srishti gupta");
+        System.out.println("srishti gupta");
     }
 }
