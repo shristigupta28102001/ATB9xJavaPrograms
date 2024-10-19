1a1b0c040c3daba4d94280831068601022ca6fee
Shristi gupta <shristig776@gmail.com>
2024-10-19 12:20:43 +0530

    commit all files

---
+++ src/Oct/ex_02102024/Lab002.java
@@ -0,0 +1,14 @@
+package Oct.ex_02102024;
+
+public class Lab002
+{
+    public static void main(String[] args) {
+        System.out.println("Calculator");
+        System.out.println("Addition -> " +"2+2"+" " +"is " +(2+2));
+        System.out.println("Substraction -> " +"4-2"+" " +"is " +(4-2));
+        System.out.println("Division -> " +"4/2"+" " +"is " +(4/2));
+        System.out.println("Multiplication ->" + "4*2"+" " +"is " +(4*2));
+
+
+    }
+}
---
+++ src/Oct/ex_05102024.java
@@ -0,0 +1,7 @@
+package Oct;
+
+public class ex_05102024 {
+    public static void main(String[] args) {
+        System.out.println("hello shristi");
+    }
+}
