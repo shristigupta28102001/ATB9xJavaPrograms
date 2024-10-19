a77abcb7a93f083c59f9bcf52856476c6df8c0fe
Shristi gupta <shristig776@gmail.com>
2024-10-19 12:27:51 +0530

    commiting changes

---
+++ src/Oct/ex_05102024Q2.java
@@ -0,0 +1,16 @@
+package Oct;
+
+public class ex_05102024Q2 {
+    public static void main(String[] args) {
+        System.out.println(" 10*1 " + ":" +  (10*1));
+        System.out.println(" 10*2 " + ":" +  (10*2));
+        System.out.println(" 10*3 " + ":" +  (10*3));
+        System.out.println(" 10*4 " + ":" +  (10*4));
+        System.out.println(" 10*5 " + ":" +  (10*5));
+        System.out.println(" 10*6 " + ":" +  (10*6));
+        System.out.println(" 10*7 " + ":" +  (10*7));
+        System.out.println(" 10*8 " + ":" +  (10*8));
+        System.out.println(" 10*9 " + ":" +  (10*9));
+        System.out.println(" 10*10 " + ":" +  (10*10));
+    }
+}
